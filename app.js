d299cf2b45d6e4150d32f88dcb4c97456b5ca4de
Luiz Alves (Hik) <luiz.alves@playkids.com>
2021-10-05 20:01:13 -0300

    Add health route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+app.get('/health', (req, res) => {
+  res.sendStatus(200)
+})
+
 app.get('/', (req, res) => {
   res.send(`Hello World from ${ip.address()} :p`)
 })
