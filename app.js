5051012eced001ac6b2d6e1617882d9f732198fe
Luiz Alves (Hik) <luiz.alves@playkids.com>
2021-10-05 18:43:46 -0300

    Big bang :boom:

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const ip = require('ip')
+const app = express()
+const port = 8080
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send(`Hello World from ${ip.address()} :p`)
+})
